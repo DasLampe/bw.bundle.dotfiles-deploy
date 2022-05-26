91ab1b1798e424da9538ad251aca2f54f9b6b278
DasLampe <daslampe@lano-crew.org>
2022-05-26 17:23:32 +0200

    Fixed: New bundlewrap version

--- metadata.py
+++ metadata.py
@@ -1,15 +1,12 @@
-from bundlewrap.metadata import DoNotRunAgain
-
-
 @metadata_reactor
 def add_apt_packages(metadata):
     if not node.has_bundle("apt"):
-        return DoNotRunAgain
+        raise DoNotRunAgain
 
     return {
         'apt': {
             'packages': {
                 'make': {'installed': True},
-           }
-        }
+            },
+        },
     }
